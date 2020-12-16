43950a6d0b0f9bcd0d1f606b7f8b2f90c8f800ce
sebasdrk17 <alexis7_hernandez14@hotmail.com>
2020-12-16 01:20:17 -0600

    Big patch, added every functionality in the game, for some reason the other commits crashed so i had to put everything in one

--- conwaysGame/App.js
+++ conwaysGame/App.js
@@ -6,80 +6,167 @@
  * @flow strict-local
  */
 
-import React, { Component, useState } from 'react';
-import ButtonStack from "./ButtonStack";
-import {
-  View,
-  TouchableHighlight,
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  Dimensions
-} from 'react-native';
+import React, { useState, useRef, useCallback } from 'react';
+import Grid from './Grid'
 import produce from 'immer';
+import {
+    View,
+    TouchableHighlight,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableOpacity
+} from 'react-native';
 
 const width = Dimensions.get("screen").width;
-const num_rows = 10;
-const num_cols = 10;
-
-const generateEmptyGrid = () => {
-  const rows = [];
-  for (let i = 0; i < num_rows; i++) {
-    rows.push(Array.from(Array(num_cols), () => 0));
-  }
-  return rows;
-}
 
 const Cell = (props) => {
-  const grid = props.grid;
-  let status = grid[props.row_index][props.col_index];
-  return (
-    <View
-      style={{
-        width: width * .10,
-        height: 35,
-        borderWidth: 1,
-        backgroundColor: status ? "#38b000" : "darkslategrey"
-      }}>
-    </View>
-  );
+    const grid = props.grid;
+    let status = grid[props.row_index][props.col_index];
+    return (
+        <View
+            style={{
+                width: width * .10,
+                height: 35,
+                borderWidth: 1,
+                backgroundColor: status ? "#38b000" : "darkslategrey"
+            }}>
+        </View>
+    );
 }
 
-
 const App: () => React$Node = () => {
-  const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid();
-  });
-  return (
-    <View style={{
-      flex: 1,
-      flexDirection: "row",
-      flexWrap: "wrap"
-    }}>
-      {grid.map((rows, row_index) => rows.map((col, col_index) =>
-        <TouchableHighlight
-          key={`${row_index}-${col_index}`}
-          onPress={() => {
-            const new_grid = produce(grid, grid_copy => {
-              grid_copy[row_index][col_index] = grid[row_index][col_index] ? 0 : 1;
-            });
-            setGrid(new_grid);
-          }}
-        >
-          <Cell row_index={row_index} col_index={col_index} grid={grid} />
-        </TouchableHighlight>
-      ))
-      }
+    const [grid, setGrid] = useState(() => {
+        return Grid.createEmptyGrid();
+    });
 
-      <View style={{
-        marginTop: 40,
-        marginLeft: 13
-      }}>
-        <ButtonStack />
-      </View>
+    const [running, setRunning] = useState(false);
+    const running_ref = useRef(running);
+    running_ref.current = running;
 
-    </View>
-  );
+    const startGame = useCallback(() => {
+        if (!running_ref.current) return;
+        setGrid(current_grid => Grid.startGame(current_grid));
+        setTimeout(startGame, 100);
+    }, []);
+
+    return (
+        <View style={{
+            flex: 1,
+            flexDirection: "row",
+            flexWrap: "wrap"
+        }}>
+            {grid.map((rows, row_index) => rows.map((col, col_index) =>
+                <TouchableHighlight
+                    key={`${row_index}-${col_index}`}
+                    onPress={() => {
+                        setGrid(Grid.mutateGrid(grid, row_index, col_index));
+                    }}
+                >
+                    <Cell row_index={row_index} col_index={col_index} grid={grid} />
+                </TouchableHighlight>
+            ))
+            }
+
+            <View style={{
+                marginTop: 40,
+                marginLeft: 13
+            }}>
+                <View
+                    style={styles.buttonContainer}
+                >
+                    <TouchableOpacity
+                        style={styles.button}
+                    >
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Start</Text>
+                    </TouchableOpacity>
+                </View>
+                <View
+                    style={styles.buttonContainer}
+                >
+                    <TouchableOpacity
+                        style={{
+                            height: 50,
+                            width: width * 0.9,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            borderRadius: 10,
+                            backgroundColor: "#38b000"
+                        }}
+                        onPress={() => {
+                            setRunning(true);
+                            if (!running) {
+                                running_ref.current = true;
+                                startGame();
+                                setRunning(false);
+                                running_ref.current = false;
+                            }
+                        }}
+                    >
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Next</Text>
+                    </TouchableOpacity>
+                </View>
+                <View
+                    style={styles.buttonContainer}
+                >
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            setRunning(true);
+                            if (!running) {
+                                running_ref.current = true;
+                                startGame();
+                            }
+                        }}
+                    >
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Auto</Text>
+                    </TouchableOpacity>
+                </View>
+                <View
+                    style={styles.buttonContainer}
+                >
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => setGrid(Grid.createEmptyGrid())}
+                    >
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Reset</Text>
+                    </TouchableOpacity>
+                </View>
+                <View
+                    style={styles.buttonContainer}
+                >
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            setRunning(false);
+                            running_ref.current = false;
+                        }}
+                    >
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Stop</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        </View >
+    );
 };
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        paddingVertical: 10
+    },
+    button: {
+        height: 50,
+        width: width * 0.9,
+        justifyContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        backgroundColor: "#38b000"
+    }
+});
 
 export default App;
--- conwaysGame/ButtonStack.js
+++ conwaysGame/ButtonStack.js
@@ -1,31 +1,31 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'
 
 const width = Dimensions.get("screen").width;
 
-function StartButton({setPagination, pagination}) {
-
-    return (
-        <View
-        style={styles.buttonContainer}
-        >
-            <TouchableOpacity 
-                style={styles.button}
-                onPress={() => setPagination(pagination + 1)}
-            >
-                <Text style={{color: "#fff", fontSize: 20}}>Start</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
-
-function NextButton({setPagination, pagination, disabled}) {
+function StartButton({ setPagination, pagination }) {
 
     return (
         <View
             style={styles.buttonContainer}
         >
-            <TouchableOpacity 
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => setPagination(pagination + 1)}
+            >
+                <Text style={{ color: "#fff", fontSize: 20 }}>Start</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+function NextButton({ setPagination, pagination, disabled }) {
+
+    return (
+        <View
+            style={styles.buttonContainer}
+        >
+            <TouchableOpacity
                 style={{
                     height: 50,
                     width: width * 0.9,
@@ -36,101 +36,100 @@ function NextButton({setPagination, pagination, disabled}) {
                     borderRadius: 10,
                     backgroundColor: disabled ? "gray" : "#38b000"
                 }}
-                // onPress={() => setPagination(pagination + 1)}
-                // disabled={disabled}
+            // onPress={() => setPagination(pagination + 1)}
+            // disabled={disabled}
             >
-                <Text style={{color: "#fff", fontSize: 20}}>Next</Text>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Next</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-function AutoButton({setPagination, pagination}) {
+function AutoButton({ setPagination, pagination }) {
 
     return (
         <View
             style={styles.buttonContainer}
         >
-            <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.button}
                 onPress={() => setPagination(pagination + 1)}
             >
-                <Text style={{color: "#fff", fontSize: 20}}>Auto</Text>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Auto</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-function ResetButton({setPagination, pagination}) {
-
+function ResetButton({ setPagination, pagination }) {
     return (
         <View
             style={styles.buttonContainer}
         >
-            <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.button}
                 onPress={() => setPagination(0)}
             >
-                <Text style={{color: "#fff", fontSize: 20}}>Reset</Text>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Reset</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-function StopButton({setPagination, pagination}) {
+function StopButton({ setPagination, pagination }) {
     return (
         <View
             style={styles.buttonContainer}
         >
-            <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.button}
                 onPress={() => setPagination(pagination + 1)}
             >
-                <Text style={{color: "#fff", fontSize: 20}}>Stop</Text>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Stop</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-function ContinueButton({setPagination, pagination}) {
+function ContinueButton({ setPagination, pagination }) {
     return (
         <View
             style={styles.buttonContainer}
         >
-            <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.button}
-                // onPress={() => setPagination(2)}
+            // onPress={() => setPagination(2)}
             >
-                <Text style={{color: "#fff", fontSize: 20}}>Continue</Text>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Continue</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
 function renderButtons(pagination, setPagination) {
-    if(pagination === 0){
-        return <StartButton pagination={pagination} setPagination={setPagination}/>
-    } else if (pagination === 1){
+    if (pagination === 0) {
+        return <StartButton pagination={pagination} setPagination={setPagination} />
+    } else if (pagination === 1) {
         return (
             <View>
-                <NextButton pagination={pagination} setPagination={setPagination}/>
-                <AutoButton pagination={pagination} setPagination={setPagination}/>
-                <ResetButton pagination={pagination} setPagination={setPagination}/>
+                <NextButton pagination={pagination} setPagination={setPagination} />
+                <AutoButton pagination={pagination} setPagination={setPagination} />
+                <ResetButton pagination={pagination} setPagination={setPagination} />
             </View>
         )
     } else if (pagination === 2) {
         return (
             <View>
-                <NextButton pagination={pagination} setPagination={setPagination} disabled={true}/>
-                <StopButton pagination={pagination} setPagination={setPagination}/>
+                <NextButton pagination={pagination} setPagination={setPagination} disabled={true} />
+                <StopButton pagination={pagination} setPagination={setPagination} />
             </View>
         )
-    } else if (pagination === 3){
+    } else if (pagination === 3) {
         return (
             <View>
-                <NextButton pagination={pagination} setPagination={setPagination} disabled={true}/>
-                <ContinueButton pagination={pagination} setPagination={setPagination}/>
-                <ResetButton pagination={pagination} setPagination={setPagination}/>
+                <NextButton pagination={pagination} setPagination={setPagination} disabled={true} />
+                <ContinueButton pagination={pagination} setPagination={setPagination} />
+                <ResetButton pagination={pagination} setPagination={setPagination} />
             </View>
         )
     }
